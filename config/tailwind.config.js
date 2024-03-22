2fe0585a4ab1f18de54247b30395734dc441d05f
Goodness I. Olajide <ifejesugoodness97@gmail.com>
2024-03-22 10:03:39 +0100

    first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        themeColorMain: "#0f2a9a",
+        themeColorSecondary: "#d23838",
+        borderColor1: "#0f2a9a",
+        textColorSecondary: "#ffffff",
+        textColorMain: "#000000",
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
